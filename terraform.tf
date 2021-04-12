d3aec3b9213be072b3ace269538c5b935439dd80
John Cena <John@gmail.com>
2021-04-12 00:58:17 -0600

    new commit

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,9 @@
 terraform {
-    backend "azurerm"
+    backend "azurerm" {
     resource_group_name = "remote-state"
     #modify the storage account name"
     storage_account_name = "terraformgopal"
     container_name = "tfstate"
     key = "web.tfstate"
+    }
 }
